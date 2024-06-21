9e7f58c10efd3ea366fdbfd89c00bff11b1f8994
Nimbonius <elijahboose08@gmail.com>
2024-06-21 15:03:44 -0400

    Added food collection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-
+import random
 pygame.init()
 
 screen = pygame.display.set_mode((1280,720))
@@ -17,6 +17,7 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.image.load('assets/fish.png').convert_alpha()
         self.image = pygame.transform.scale(self.image, (screen.get_width()/10,screen.get_height()/10))
         self.rect = self.image.get_rect()
+        self.rect.size = self.image.get_size()
 
 class Food(pygame.sprite.Sprite):
     def __init__(self):
@@ -25,6 +26,8 @@ class Food(pygame.sprite.Sprite):
         self.image = pygame.image.load('assets/food.png').convert_alpha()
         self.image = pygame.transform.scale(self.image, (screen.get_width()/10,screen.get_height()/10))
         self.rect = self.image.get_rect()
+        self.rect.size = self.image.get_size()
+        self.pos = pygame.Vector2(random.randint(0, screen.get_width()), random.randint(0, screen.get_height()))
 
 
 fish = Player()
@@ -39,11 +42,14 @@ while running:
     screen.fill("darkgreen")
     player_sprite = pygame.Rect(50, 50, 25, 25)
     player_sprite.center = player_pos
-    screen.blit(food.image, pygame.Vector2(screen.get_width() / 4 , screen.get_height() / 4) )
+    screen.blit(food.image,  food.pos)
+    food.rect.center = food.pos
     screen.blit(fish.image, player_pos)
+    fish.rect.center = player_pos
     
     if pygame.sprite.collide_rect(fish,food): #checks for collision with fish food
         print("collided")
+        food.pos = pygame.Vector2(random.randint(0, screen.get_width()), random.randint(0, screen.get_height()))
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
         player_pos.y -= 300 * dt
