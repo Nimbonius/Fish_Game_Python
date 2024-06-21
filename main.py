7414f85449780310dfb0745bf735c82734c00182
Nimbonius <elijahboose08@gmail.com>
2024-06-21 15:56:30 -0400

    Added function to keep sprites inside of the screen

--- main.py
+++ main.py
@@ -12,6 +12,23 @@ dt = 0
 
 player_pos = pygame.Vector2(screen.get_width() / 2 , screen.get_height() / 2) 
 
+def checkScreenPosition(pos, rect): #Keeps positions inside of the visible screen
+    
+    #Keep X position in screen
+    if pos[0] < 0:
+        pos[0] = 0
+    elif pos[0] > screen.get_width() - rect.width:
+        pos[0] = (screen.get_width() - rect.width)
+
+    if pos[1] < 0:
+        pos[1] = 0
+    elif pos[1] > screen.get_height() - rect.height:
+        pos[1] = (screen.get_height() - rect.height)
+    return pos
+
+
+
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -47,9 +64,9 @@ while running:
     screen.fill("darkgreen")
     player_sprite = pygame.Rect(50, 50, 25, 25)
     player_sprite.center = player_pos
-    screen.blit(food.image,  food.pos)
+    screen.blit(food.image,  checkScreenPosition(food.pos, food.rect))
     food.rect.center = food.pos
-    screen.blit(fish.image, player_pos)
+    screen.blit(fish.image, checkScreenPosition(player_pos, fish.rect))
     fish.rect.center = player_pos
     text_surface = my_font.render(str(foodTotal), False, (0,0,0))
     screen.blit(text_surface, (0,0))
