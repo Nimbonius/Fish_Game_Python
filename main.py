f1b4741f52b88c2ac3d9b7dd4dbd29682ab2dd47
Nimbonius <elijahboose08@gmail.com>
2024-06-23 13:50:52 -0400

    Changed Food Asset and added upgrade button for food collection

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ pygame.font.init()
 
 my_font = pygame.font.SysFont('Comic Sans MS', 30)
 
-screen = pygame.display.set_mode((1280,720))
+screen = pygame.display.set_mode((1280/2,720/2))
 clock = pygame.time.Clock()
 running = True
 dt = 0
@@ -52,9 +52,26 @@ class Food(pygame.sprite.Sprite):
         self.rect.size = self.image.get_size()
         self.pos = pygame.Vector2(random.randint(0, screen.get_width()), random.randint(0, screen.get_height()))
 
+class Button():
+    def __init__(self, rect_size, rect_pos, text):
+        #self.rect = pygame.Rect(rect_pos[0], rect_pos[1], rect_size[0], rect_size[1])
+        self.text = my_font.render(str(text), False, (0,0,0))
+        self.rect = self.text.get_rect()
+        self.rect.center = checkScreenPosition(rect_pos, self.rect)
+
+    def render(self):
+        self.rendered_rect = pygame.draw.rect(screen, (255,0,0), self.rect)
+        screen.blit(self.text, (self.rect.x, self.rect.y))
+    
+    
+        
+        
 
 fish = Player()
 food = Food()
+foodIncrement = 1
+foodPrice = 10
+upgradeButton = Button((50,50), [screen.get_width()/2, screen.get_height()], "Upgrade")
 
 foodTotal = 0
 heading_x = True # True if the fish in heading in the x-positive direciton
@@ -63,9 +80,24 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN and pygame.BUTTON_LEFT:
+            if upgradeButton.rect.collidepoint(pygame.mouse.get_pos()):
+                if foodTotal >= foodPrice:
+                    foodTotal -= foodPrice
+                    foodIncrement += 1
+                    foodPrice = foodIncrement*foodIncrement + 10
+                    print(foodTotal, foodIncrement, foodPrice)
+                    
 
     
-    screen.fill("darkgreen")
+    screen.fill("blue")
+
+    # Buttons
+
+    upgradeButton.render() 
+
+    #Sprites
+
     player_sprite = pygame.Rect(50, 50, 25, 25)
     player_sprite.center = player_pos
     screen.blit(food.image,  checkScreenPosition(food.pos, food.rect))
@@ -73,11 +105,16 @@ while running:
     screen.blit(fish.image, checkScreenPosition(player_pos, fish.rect))
     fish.rect.center = player_pos
     text_surface = my_font.render(str(foodTotal), False, (0,0,0))
+
+    # Text
     screen.blit(text_surface, (0,0))
+
+    
+    
     
 
     if pygame.sprite.collide_rect(fish,food): #checks for collision with fish food
-        foodTotal += 1
+        foodTotal += foodIncrement
         food.pos = pygame.Vector2(random.randint(0, screen.get_width()), random.randint(0, screen.get_height()))
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
@@ -97,7 +134,7 @@ while running:
     
 
 
-    pygame.display.flip()
+    pygame.display.flip() #Displays new Information
 
     dt = clock.tick(60)/1000
 
